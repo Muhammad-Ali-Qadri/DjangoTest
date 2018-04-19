3cc4a92a4db4a1e0c42f3856990e00ed6372efb0
Muhammad Ali Qadri <aliqadri1234@gmail.com>
2018-04-19 23:09:20 +0500

    initial 4

--- djangoTest/settings.py
+++ djangoTest/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "polls/static"),
 ]
 
 STATIC_URL = '/static/'
