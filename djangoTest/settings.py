aafc35bcc7facc7f30dc17d8d22c0db0bd7be421
Muhammad Ali Qadri <aliqadri1234@gmail.com>
2018-04-20 23:55:32 +0500

    initial 4

--- djangoTest/settings.py
+++ djangoTest/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
-import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -79,7 +78,6 @@ WSGI_APPLICATION = 'djangoTest.wsgi.application'
 
 DATABASES = {
     'default': {
-        dj_database_url.config(ssl_require=True),
     }
 }
 
@@ -135,3 +133,7 @@ MIDDLEWARE_CLASSES = [
 ]
 
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+import dj_database_url
+
+DATABASES['default'] = dj_database_url.config(ssl_require=True)
