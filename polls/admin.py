8e9522a9518cd29753e2b1118f7c5cbbdc264159
Muhammad Ali Qadri <aliqadri1234@gmail.com>
2018-04-22 15:16:02 +0500

    created models and registered models

--- polls/admin.py
+++ polls/admin.py
@@ -1,10 +1,31 @@
 from django.contrib import admin
 from .models import *
+from django import forms
 # Register your models here.
 
-admin.site.register(Room)
 admin.site.register(Type)
-admin.site.register(Images)
 admin.site.register(RestaurantUser)
 admin.site.register(Registration)
 admin.site.register(RegistrationDetails)
+
+
+# admin class for defining room interface
+@admin.register(Room)
+class RoomAdmin(admin.ModelAdmin):
+    list_display = ('room_number', 'room_type')
+    pass
+
+
+# display form for adding images
+class AdminImagesForm(forms.ModelForm):
+    class Meta:
+        fields = ['Image', 'room_type_id', 'description']
+        widgets = {
+            'Image': forms.ImageField()
+        }
+
+
+# admin class for defining images interface
+@admin.register(Images)
+class ImagesAdmin(admin.ModelAdmin):
+    form = AdminImagesForm
