01336a1166113034a212c998e3dcae44baa17f74
Muhammad Ali Qadri <aliqadri1234@gmail.com>
2018-04-23 23:40:45 +0500

    added profile and logout skeleton

--- polls/models.py
+++ polls/models.py
@@ -97,7 +97,7 @@ def create_user_profile(sender, instance, created, **kwargs):
 
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
-    instance.profile.save()
+    instance.hoteluserprofile.save()
 
 
 class Registration(models.Model):
